7596fab932acfdda20583f386f22961e98014bd7
weirui@1234 <weirui@xiaowangcloud.cn>
2023-09-18 17:56:13 +0800

    add task status color

--- chat_app/admin.py
+++ chat_app/admin.py
@@ -8,11 +8,27 @@ admin.site.site_title = '大江狗管理后台'  # 设置title
 admin.site.index_title = '大江狗管理后台'
 
 from .models import Task, Status
+from django.utils.html import format_html
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('name', 'status_text')
+    list_display = ('colored_name', 'status_text')
+
+    def colored_name(self, obj):
+        color_code = 'black'
+        if obj.status == Status.UNSTARTED:
+            color_code = 'red'
+        elif obj.status == Status.ONGOING:
+            color_code = 'orange'
+        elif obj.status == Status.FINISHED:
+            color_code = 'green'
+        return format_html('<span style="color: {};">{}</span>', color_code, obj.name)
+    colored_name.short_description = '任务名称'
 
     def status_text(self, obj):
         return dict(Status.choices)[obj.status]
     status_text.short_description = '任务状态'
+
+
+
+
--- chat_app/models.py
+++ chat_app/models.py
@@ -11,8 +11,8 @@ class Status(models.TextChoices):
 
 
 class Task(models.Model):
-    name = models.CharField(verbose_name="Task name", max_length=65, unique=True)
-    status = models.CharField(verbose_name="Task status", max_length=1, choices=Status.choices)
+    name = models.CharField(verbose_name="任务名称", max_length=65, unique=True)
+    status = models.CharField(verbose_name="任务状态", max_length=1, choices=Status.choices)
 
     class Meta:
         verbose_name = "任务"
